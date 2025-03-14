9d6d577920588f60a3f6cde466539492c15c1b7b
ottozip <ottozipx@gmail.com>
2025-03-14 23:55:19 +0900

    feat(#58): ObjectionController 구현

--- src/main/java/com/shiftm/shiftm/domain/objection/api/ObjectionController.java
+++ src/main/java/com/shiftm/shiftm/domain/objection/api/ObjectionController.java
@@ -1,4 +1,23 @@
 package com.shiftm.shiftm.domain.objection.api;
 
+import com.shiftm.shiftm.domain.objection.dto.request.CreateObjectionReq;
+import com.shiftm.shiftm.domain.objection.service.ObjectionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/objection")
+@RestController
 public class ObjectionController {
+
+    private final ObjectionService objectionService;
+
+    @PostMapping
+    public void createObjection(@Valid @RequestBody final CreateObjectionReq requestDto) {
+        objectionService.createObjection(requestDto);
+    }
 }
--- src/main/java/com/shiftm/shiftm/domain/objection/service/ObjectionService.java
+++ src/main/java/com/shiftm/shiftm/domain/objection/service/ObjectionService.java
@@ -1,4 +1,16 @@
 package com.shiftm.shiftm.domain.objection.service;
 
+import com.shiftm.shiftm.domain.objection.dto.request.CreateObjectionReq;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class ObjectionService {
+
+    @Transactional
+    public void createObjection(final CreateObjectionReq requestDto) {
+
+    }
 }
