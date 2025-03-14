ce9e538d91db5f0e50d77b5660f8c73f28b4576a
ottozip <ottozipx@gmail.com>
2025-03-14 23:50:56 +0900

    feat(#58): CreateObjectionReq Dto 구현

---
+++ src/main/java/com/shiftm/shiftm/domain/objection/dto/request/CreateObjectionReq.java
@@ -0,0 +1,16 @@
+package com.shiftm.shiftm.domain.objection.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record CreateObjectionReq(
+        @NotNull
+        Long shiftId,
+        @NotBlank
+        String type,
+        @NotNull
+        LocalDateTime updatedTime
+) {
+}
